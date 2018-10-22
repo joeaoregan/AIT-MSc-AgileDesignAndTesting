f652a21bc05897d60cb6fbce09e512f130244d26
Joe O'Regan <joeaoregan@yahoo.ie>
2018-10-22 12:52:30 +0100

    Tidy code

--- Tests/com/ait/stock/StockTest.java
+++ Tests/com/ait/stock/StockTest.java
@@ -4,36 +4,22 @@
  */
 package com.ait.stock;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 class StockTest {
 
 	private StockBroker stockBroker = mock(StockBroker.class);	// Create StockBroker instance using Mockito
-//	private StockListener stockListener = mock(StockListener.class);
-	private StockListener stockListener = new StockListener(stockBroker);
-	private Stock stock = new Stock("stock1", 100.0);
-//	private Stock stock2 = new Stock("stock1", 50.0);
-	//private StockListener stockListener2 = mock(StockListener.class);
+	private StockListener stockListener;
+	private Stock stock;
 	
-	@Test
-	void testStock() {
-		assertTrue(stockBroker instanceof StockBroker);
+	@BeforeEach
+	public void setup() {
+		stockListener = new StockListener(stockBroker);
+		stock = new Stock("stock1", 100.0);
 	}
-	
-	
-//	@Test
-//	void stockListenerTest() {	
-//		stockListener.takeAction(stock);
-//		verify(stockBroker).buy(stock,100);
-//		
-//		when(stockBroker.getQoute(stock)).thenReturn(120.0);
-//		stockListener.takeAction(stock);
-//		verify(stockBroker).sell(stock,10);
-//	}
-	
+		
 	@Test
 	void testBuyStockWhenPriceGoesDown() {
 		when(stockBroker.getQoute(stock)).thenReturn(90.0);
